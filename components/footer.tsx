9d53adccf582c43d6c3be39ef14cb6b35f12df5f
MR.PICKLE <xkpomx@gmail.com>
2025-12-26 12:49:39 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
   const handleLogin = async () => {
     await signIn.social({
       provider: "google",
-      callbackURL: "/register",
+      callbackURL: "/", //change later
     });
   };
 
