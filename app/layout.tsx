f9221c8181e20915cac6a452e146b5bb019f3978
adisuarn <adisuarn.w@gmail.com>
2026-01-05 18:26:20 +0700

    add metadata, add checkin schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
 import {
   Geist,
@@ -8,12 +7,16 @@ import {
   Bai_Jamjuree,
 } from "next/font/google";
 
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "../components/header";
 import HeaderDev from "@/components/headerdev";
 import Footer from "../components/footer";
 import Providers from "@/components/provider";
+import favicon from '../public/favicon.ico'
+import ogImage from '../public/ogImage.png'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,6 +54,12 @@ export const metadata: Metadata = {
     default: "Triam Udom Open House 2026",
     template: "%s | Triam Udom Open House 2026",
   },
+  keywords: [
+    "Open House", "Triam Udom", "เตรียมอุดมศึกษา", "งานนิทรรศการ", "กิจกรรม", "แนะแนวการศึกษา", "เปิดบ้านวิชาการ", "TU Open House", "เตรียมอุดม", "Triam Udom Open House 2026",
+    "TU", "โรงเรียนเตรียมอุดมศึกษา", "สอบเข้า", "Open House 2026", "TU Open House 2026"
+  ],
+  authors: [{ name: "Triam Udom Suksa School", url: "https://triamudom.ac.th" }],
+  icons: [{ rel: 'icon', url: favicon.src }],
   description:
     "88 ปีงานนิทรรศการวิชาการเตรียมอุดมศึกษา (Triam Udom Open House 2026)",
   metadataBase: new URL("https://openhouse.triamudom.ac.th"),
@@ -60,12 +69,29 @@ export const metadata: Metadata = {
       "88 ปีงานนิทรรศการวิชาการเตรียมอุดมศึกษา (Triam Udom Open House 2026)",
     url: "https://openhouse.triamudom.ac.th",
     siteName: "Triam Udom Open House 2026",
-    // images: ["/og.png"],
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      }
+    ],
     type: "website",
   },
-  // twitter: {
-  //   card: "summary_large_image",
-  // },
+  twitter: {
+    card: "summary_large_image",
+    title: 'Triam Udom Open House 2025',
+    site: '@triamudomoph',
+    description:
+      'งานนิทรรศการประจำปีของโรงเรียนเตรียมอุดมศึกษา พบกับกิจกรรมที่น่าสนใจจากนักเรียนโรงเรียนเตรียมอุดมศึกษา อาทิ แนะแนวการศึกษาต่อ แนะนำสายการเรียน และกิจกรรมจากชมรมต่าง ๆ',
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
