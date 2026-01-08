3b5ccd7c04219c79f0e50d4e8d56b6824ea9d52f
beta160900 <beta010js@gmail.com>
2026-01-08 12:49:59 +0700

    cm

--- app/backend/page.tsx
+++ app/backend/page.tsx
@@ -1,14 +1,22 @@
-const Backend = () => {
-  return (
-    <div className="w-full bg-[linear-gradient(0deg,#0B1855_-3.97%,#042284_35.32%,#3450B0_100%),linear-gradient(180deg,#042284_1.28%,#3450B0_11.64%,#F3E09D_29.58%),linear-gradient(180deg,#457BCA_-6.32%,#3450B0_54.58%,#042284_100%)] ">
-      <div className=" w-[500px] mx-auto flex flex-col ">
-        <div className=" w-[90%] flex flex-col bg-white/10">
-          <p>วันเสาร์</p>
-          <p>10 มกราคม พ.ศ. 2569</p>
-          <p>20 : 20 : 20</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-export default Backend;
+// const Backend = () => {
+//   return (
+//     <div className="w-full bg-[linear-gradient(0deg,#0B1855_-3.97%,#042284_35.32%,#3450B0_100%),linear-gradient(180deg,#042284_1.28%,#3450B0_11.64%,#F3E09D_29.58%),linear-gradient(180deg,#457BCA_-6.32%,#3450B0_54.58%,#042284_100%)] ">
+//       <div className=" w-[500px] mx-auto flex flex-col ">
+//         <div className=" w-[90%] flex flex-col bg-white/10">
+//           <p>วันเสาร์</p>
+//           <p>10 มกราคม พ.ศ. 2569</p>
+//           <p>20 : 20 : 20</p>
+//         </div>
+//         <div>
+//             <p>จำนวนคนลงทะเบียน</p>
+//             <p>10,000 คน</p>
+//         </div>
+//         <div>
+//             <p>จำนวนคนลงทะเบียน</p>
+//             <p>10,000 คน</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Backend;
