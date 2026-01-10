624a841faea0374a14f800bf781c9bdb8ad4510c
mineners <76274288+minenersGaming@users.noreply.github.com>
2026-01-10 21:02:03 +0700

    resolved line break error

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -78,9 +78,9 @@ export default function Home() {
             <RegisterText className=" z-40 absolute w-[270px] -top-5 sm:w-[270px] sm:-top-5 md:w-[320px] md:-top-12 lg:w-[400px] lg:-top-14 h-auto" />
             <GoogleBu
               tton
-              // onClick={handleLogin}
               className="hidden lg:block cursor-not-allowed z-50 absolute w-[270px] top-19 sm:top-19 sm:w-[270px] md:w-[320px] md:top-16 lg:w-[400px] lg:top-20 hover: scale-105 transition-all h-auto "
             />
+            <GoogleButton className="hidden lg:block cursor-not-allowed z-50 absolute w-[270px] top-19 sm:top-19 sm:w-[270px] md:w-[320px] md:top-16 lg:w-[400px] lg:top-20 hover: scale-105 transition-all h-auto " />
             <TucmcLogo className=" z-40 absolute w-[210px] top-38 sm:w-[210px] sm:top-38 md:w-[230px] md:top-39 lg:w-[280px] lg:top-48 h-auto" />
           </div>
         </div>
