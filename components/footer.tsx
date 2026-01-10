4a2dfed51b4ee69492914e4459d52b56ab6de786
mineners <76274288+minenersGaming@users.noreply.github.com>
2026-01-10 21:08:51 +0700

    resolved syntax

--- components/footer.tsx
+++ components/footer.tsx
@@ -78,13 +78,7 @@ const Footer = () => {
                 >
                   ออกจากระบบ
                 </button>
-              ) : (
-                // <button
-                //   onClick={handleLogin}
-                //   className="bg-linear-to-r from-[#F3E19D] via-[#F4F2C4] to-[#E6C674] text-[#062078] rounded-full px-3 py-1 lg:px-5 lg:py-1.5 lg:text-lg text-center cursor-pointer mt-2 font-bold hover:scale-110 transition-all relative z-10"
-                // >
-                //   เข้าสู่ระบบ
-                // </button>         
+              ) : (               
                 <a
                   href="/register"
                   className="bg-linear-to-r from-[#F3E19D] via-[#F4F2C4] to-[#E6C674] text-[#062078] rounded-full px-3 py-1 lg:px-5 lg:py-1.5 lg:text-lg text-center cursor-pointer mt-2 font-bold hover:scale-110 transition-all relative z-10"
